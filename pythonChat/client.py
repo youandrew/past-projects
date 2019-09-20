8746815d2e0fd70b72b61f73fa83612fa26523d2
Andrew the XII <54558003+youandrew@users.noreply.github.com>
2019-09-19 20:52:51 -0500

    not gonna lie
    i didnt test em since i dont have two compouters

--- pythonChat/client.py
+++ pythonChat/client.py
@@ -12,37 +12,37 @@ player = socket.socket()
 serverString = input("Enter name of host")
 port = input("Enter port number")
 player.connect(serverString,port)
+
+textarray = []
+font = pygame.font.Font("Arial",20)
+
 thread1 = Thread(chat)
 thread2 = Thread(chatDisplay)
+thread3 = Thread(receive)
+
+def recieve():
+    while Run:
+        text = player.recv(4028)
+        textarray.append(text)
 def chat():
     while Run:
         msg = input("Enter Text to send")
+        player.send(msg)
 def chatDisplay():
     
     
     while Run:
         
-        for event in pygame.event.get():#this is similiar to making a window in cpp events are like mouse clicks or keyboard presses they all get queued here
-            if event.type == pygame.QUIT:#pygame.quit means the user hit the x button on the top right
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 gameRun = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    isLeft = True
-                if event.key == pygame.K_RIGHT:#keyup means the user lifted their finger off the key
-                    isRight = True
-            elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_LEFT:
-                    isLeft = False
-                if event.key == pygame.K_RIGHT:
-                    isRight = False
-        if(isLeft):
-            x = x -1
-        if(isRight):
-            x = x + 1
-        screen.fill((255,0,0))                   #these four numbers are left top width height
-        pygame.draw.rect(screen, (255,255,255), [x,200,100,60],0)
-        pygame.display.flip()# this flip is refering to how opengl(the low level language for your motherboard and shit) renders graphics it basically "flips" the image just know you need this to have the screen update
+            
+        screen.fill((255,0,0))
+        y = 0
+        for t in range(len(textarray)):
+            text = font.render(textarray[t],True,(0,0,0))
+            screen.blit(text,(0,y))
+            y += 50
+        pygame.display.flip()
         clock.tick(24)#this is framerate
     pygame.quit()
-
-    
